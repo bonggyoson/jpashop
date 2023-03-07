26c6a80736493184c61502ae53c1506fa1efa541
bonggyo son <bonggyoson@gmail.com>
2023-03-07 18:30:49 +0900

    API 개발 회원 등록 복습

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -18,6 +18,37 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+
     @GetMapping("/api/v1/members")
     public List<Member> membersV1() {
         return memberService.findMembers();
@@ -45,22 +76,6 @@ public class MemberApiController {
         private String name;
     }
 
-    @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
-        Long id = memberService.join(member);
-        return new CreateMemberResponse(id);
-    }
-
-    @PostMapping("/api/v2/members")
-    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
-
-        Member member = new Member();
-        member.setName(request.getName());
-
-        Long id = memberService.join(member);
-        return new CreateMemberResponse(id);
-    }
-
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV3(@PathVariable("id") Long id,
                                                @RequestBody @Valid UpdateMemberRequest request) {
@@ -81,18 +96,4 @@ public class MemberApiController {
         private String name;
     }
 
-
-    @Data
-    static class CreateMemberRequest {
-        private String name;
-    }
-
-    @Data
-    static class CreateMemberResponse {
-        private Long id;
-
-        public CreateMemberResponse(Long id) {
-            this.id = id;
-        }
-    }
 }
