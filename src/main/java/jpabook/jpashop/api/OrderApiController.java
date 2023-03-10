2b5d72e22cae9a87118962f151c408fb15867246
bonggyo son <bonggyoson@gmail.com>
2023-03-10 10:01:02 +0900

    엔티티를 DTO로 변환 - 페치 조인 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -25,19 +25,31 @@ public class OrderApiController {
     private final OrderRepository orderRepository;
     private final OrderQueryRepository orderQueryRepository;
 
+    /**
+     * V1. 엔티티 직접 노출
+     * - 엔티티가 변하면 API 스펙이 변한다.
+     * - 트랜잭션 안에서 지연 로딩 필요
+     * - 양방향 연관관계 문제
+     */
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+
         for (Order order : all) {
-            order.getMember().getName();
-            order.getDelivery().getAddress();
+            order.getMember().getName();        // Lazy 강제 초기화
+            order.getDelivery().getAddress();   // Lazy 강제 초기화
+
             List<OrderItem> orderItems = order.getOrderItems();
-            orderItems.stream().forEach(o -> o.getItem().getName());
+            orderItems.stream().forEach(o -> o.getItem().getName());    // Lazy 강제 초기화
         }
 
         return all;
     }
 
+    /**
+     * * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * * - 트랜잭션 안에서 지연 로딩 필요
+     */
     @GetMapping("/api/v2/orders")
     public List<OrderDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
@@ -47,9 +59,18 @@ public class OrderApiController {
         return result;
     }
 
+    /**
+     * * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - 페이징 시에는 N 부분을 포기해야함
+     * - (대신에 batch fetch size? 옵션 주면 N -> 1 쿼리로 변경 가능)
+     */
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
+
+        for (Order order : orders) {
+            System.out.println("order ref= " + order + " id=" + order.getId());
+        }
 
         List<OrderDto> result = orders.stream().map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
@@ -91,16 +112,18 @@ public class OrderApiController {
             orderDate = order.getOrderDate();
             orderStatus = order.getStatus();
             address = order.getDelivery().getAddress();
-            orderItems = order.getOrderItems().stream().map(orderItem -> new OrderItemDto(orderItem)).collect(Collectors.toList());
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
         }
     }
 
     @Getter
     static class OrderItemDto {
 
-        private String itemName;
-        private int orderPrice;
-        private int count;
+        private String itemName;    // 상품 명
+        private int orderPrice;     // 주문 가격
+        private int count;          // 주문 수량
 
         public OrderItemDto(OrderItem orderItem) {
             itemName = orderItem.getItem().getName();
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.item.Item;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import lombok.Setter;
 
 import javax.persistence.*;
